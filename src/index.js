c1533644653b31ca2b424b6f033ea06e1f36a18f
Shine <shine002@gmail.com>
2020-03-08 13:19:21 -0600

    React proj

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Portfolio from './portfolio';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Portfolio>Hello</Portfolio>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Portfolio() {
+    return (
+        <div>
+        <h1>Shine Kaippilly</h1>
+          <p>Junior Full-Stack Web Developer.</p>
+        </div>
+        
+    );
+  }
+  
+  export default Portfolio;
